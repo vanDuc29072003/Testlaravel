0b8653430e81bf4a4788a26e5106bc646a975f50
Nhatanhvb <nhatanhvb@gmail.com>
2025-04-10 15:01:43 +0700

    refactor: reorganize CSS and JS includes for better structure

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,19 +3,12 @@
 <html lang="en">
 
 <head>
-  <!-- Các tệp CSS/JS khác -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-notify/0.2.0/js/bootstrap-notify.min.js"></script>
-  <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
-
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title', 'CTY TNHH IN T.KHOA')</title>
   <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/x-icon" />
-
   <!-- Fonts and icons -->
   <script src="{{ asset('js/plugin/webfont/webfont.min.js') }}"></script>
   <script>
@@ -35,13 +28,17 @@
       },
     });
   </script>
-
   <!-- CSS Files -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/plugins.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/kaiadmin.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/demo.css') }}">
+  <!-- Các tệp CSS/JS khác -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-notify/0.2.0/js/bootstrap-notify.min.js"></script>
+  <script src="https://js.pusher.com/8.4.0/pusher.min.js"></script>
 </head>
 
 <body style="zoom: 0.85;">
