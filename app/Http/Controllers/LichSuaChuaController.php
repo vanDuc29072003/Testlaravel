c52a5439709ac9c6e26df41052c6300865f4021d
Nhatanhvb <nhatanhvb@gmail.com>
2025-04-11 16:18:24 +0700

    refactor: merge lai

--- app/Http/Controllers/LichSuaChuaController.php
+++ app/Http/Controllers/LichSuaChuaController.php
@@ -4,13 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\LichSuaChua;
 use Illuminate\Http\Request;
-use App\Models\LichSuaChua;
 use App\Models\YeuCauSuaChua;
 use App\Models\NhanVien;
 
 class LichSuaChuaController extends Controller
 {
-<<<<<<< HEAD
     public function index(Request $request) {
         $query = LichSuaChua::query();
     
@@ -32,18 +30,5 @@ class LichSuaChuaController extends Controller
         $dsNhanVien = NhanVien::all();
     
         return view('vLichSuaChua.lichsuachua', compact('dsLichSuaChua', 'dsNhanVien'));
-=======
-    public function index()
-    {
-        $lichsuachua = LichSuaChua::with(
-            [
-            'yeucau.nhanvienyeucau',
-            'yeucau.may',
-            'nhanvienkithuat'
-            ]
-        )->get();
-            
-        return view('lichsuachua', compact('lichsuachua'));
->>>>>>> 5096e39f7c9cc50257838bee1acdd0e531f451e1
     }
 }
--- app/Models/LichSuaChua.php
+++ app/Models/LichSuaChua.php
@@ -8,32 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class LichSuaChua extends Model
 {
     use HasFactory;
-    protected $table = 'lichsuachua';
-    protected $fillable = [
-        
-
-    ];
-    function yeucau()
-    {
-        return $this->belongsTo(YeuCauSuaChua::class, 'MaYeuCauSuaChua', 'MaYeuCauSuaChua');
-
-    }
-    function nhanvienkithuat()
-    {
-        return $this->belongsTo(NhanVien::class, 'MaNhanVienKyThuat', 'MaNhanVien');
-    }
-}
-
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class LichSuaChua extends Model
-{
-    use HasFactory;
 
     protected $table = 'lichsuachua';
     protected $primaryKey = 'MaLichSuaChua';
